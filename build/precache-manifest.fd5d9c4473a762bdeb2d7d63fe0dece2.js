004f4c756c9c3bf0a8a5f474821bbc0fa7a0364d
rosie holliday <rosiemcposie@gmail.com>
2020-12-08 11:59:18 +0000

    build

---
+++ build/precache-manifest.fd5d9c4473a762bdeb2d7d63fe0dece2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1567584124be2dfdc6b07f8ef6bc7d80",
+    "url": "/react-resume/index.html"
+  },
+  {
+    "revision": "b720cd89dd04d89f36c3",
+    "url": "/react-resume/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "0d512c60dd2ffe2c8c49",
+    "url": "/react-resume/static/js/2.3e63ca66.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/react-resume/static/js/2.3e63ca66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b720cd89dd04d89f36c3",
+    "url": "/react-resume/static/js/main.51dc7738.chunk.js"
+  },
+  {
+    "revision": "602b45e7819df3352f5d",
+    "url": "/react-resume/static/js/runtime-main.bbf3433e.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
 
   getResumeData(){
     $.ajax({
-      url:'/resumeData.json',
+      url:'data/resumeData.json',
       dataType:'json',
       cache: false,
       success: function(data){
